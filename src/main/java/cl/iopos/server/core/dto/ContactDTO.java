071a9527bc2a9dbede5275f7dfefe21526d4ae17
Cristian <Cristian@cfuentes-np870>
2017-06-02 02:17:11 -0300

    commit

---
+++ src/main/java/cl/iopos/server/core/dto/ContactDTO.java
@@ -0,0 +1,59 @@
+package cl.iopos.server.core.dto;
+
+import java.io.Serializable;
+
+public class ContactDTO implements Serializable {
+
+	private static final long serialVersionUID = 7552365241124514344L;
+
+	private String firstName;
+	
+	private String lastName;
+
+	private String mobileNumber;
+	
+	private String telephoneNumber;
+	
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
